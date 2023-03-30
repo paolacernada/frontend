b4f0c89bf21f4eb3a48301994cf8ad0cb2285507
Paola Cernada <96987381+paolacernada@users.noreply.github.com>
2023-03-29 21:06:28 -0400

    change

--- src/pages/CourseLogPage.js
+++ src/pages/CourseLogPage.js
@@ -23,15 +23,23 @@ function CourseLogPage({ setCourse }) {
 
     const deleteCourse = async id => {
 
-        const response = await fetch(`/courseRegistry/${id}`, { method: "DELETE"});
+        const response = await fetch("https://paolacernada-website.herokuapp.com", {
+            method: "DELETE", 
+            body: JSON.stringify({id:`${id}`}), 
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
 
-        if (response.status === 204) {
-            const getResponse = await fetch("https://paolacernada-website.herokuapp.com");
-            const courses = await getResponse.json();
-            setCourses(courses);
-        } else {
-            console.error(`Failed to delete the row with id: ${id} (status code = 204)`)
-        }
+        // const response = await fetch(`/courseRegistry/${id}`, { method: "DELETE"});
+
+        // if (response.status === 204) {
+        //     const getResponse = await fetch("https://paolacernada-website.herokuapp.com");
+        //     const courses = await getResponse.json();
+        //     setCourses(courses);
+        // } else {
+        //     console.error(`Failed to delete the row with id: ${id} (status code = 204)`)
+        // }
     }
 
     useEffect(() => {
